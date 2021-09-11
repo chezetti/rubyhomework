16250664bd161270b6c90dba76ed626a29dc8cda
chezetti <riza25@bk.ru>
2021-09-11 12:49:18 +0300

    added menu and modified tests

--- First task/task1.rb
+++ First task/task1.rb
@@ -1,30 +1,29 @@
-# frozen_string_literal: true
+  # frozen_string_literal: true
 
-# Task 1.
-class Greetings
-  def self.helloMaker
+  # Task 1.
+  def helloMaker
     puts '-' * 80
     puts 'Доброго времени суток.'
     puts '-' * 80
     puts 'Введите имя: '
-    name = gets
+    input_name = gets
     puts 'Введите фамилию: '
-    surname = gets
+    input_surname = gets
     puts 'Введите возраст: '
-    age = gets.to_i
+    input_age = gets.to_i
     puts '-' * 80
-    if age.to_i < 18
-      puts "Привет, #{name.capitalize} #{surname.capitalize}.\nТебе меньше 18 лет, но начать учиться программировать никогда не рано"
+    if input_age.to_i < 18
+      puts "Привет, #{input_name.capitalize} #{input_surname.capitalize}.\nТебе меньше 18 лет, но начать учиться программировать никогда не рано"
+      "Привет, #{input_name.capitalize} #{input_surname.capitalize}.\nТебе меньше 18 лет, но начать учиться программировать никогда не рано"
     else
-      puts "Привет, #{name} #{surname}. \nСамое время заняться делом!"
+      puts "Привет, #{input_name} #{input_surname}. \nСамое время заняться делом!"
+      "Привет, #{input_name} #{input_surname}. \nСамое время заняться делом!"
     end
-    puts '-' * 80
   end
-end
 
-# Task 2.
-class Foobar
-  def self.fooBar
+  # Task 2.
+  def fooBar
+    puts '-' * 80
     puts 'Введите пару чисел: '
     figures = []
     (0..1).each do |i|
@@ -33,16 +32,37 @@ class Foobar
     flag = false
     figures.each do |figure|
       if figure == 20
-        puts "Второе введёное число равно: #{figures[1]}"
         flag = true
+        puts "Второе введёное число равно: #{figures[1]}"
+        return "Второе введёное число равно: #{figures[1]}"
       end
     end
     if flag == false
       sum = 0
       figures.each { |figure| sum += figure }
       puts "Сумма всех элементов равна: #{sum}"
+      "Сумма всех элементов равна: #{sum}"
     end
   end
-end
+
+  def menu
+    loop do
+      puts "Выбор задания :
+      1. Первое задание
+      2. Второе задание
+      -1. Выход"
+      input = gets.to_i
+      if input == 1
+        helloMaker
+      elsif input == 2
+        fooBar
+      elsif input == -1
+      break
+      else
+        puts "Команда не найдена."
+      end
+    end
+  end
+
 
 
